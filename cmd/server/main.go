4c4998722d6a40e44fda9fba11129118426e9711
Alexander Goussas <goussasalexander@gmail.com>
2025-04-08 15:50:40 -0500

    feat: add endpoint for retrieving single configuration value;

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,6 +14,7 @@ func handleGetAll(w http.ResponseWriter, r *http.Request) {
 
 	cfg, ok := config.GetAll(config.Env(env))
 	if !ok {
+		log.Printf("Failed to get configuration values for env: %s\n", env)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -21,9 +22,29 @@ func handleGetAll(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(cfg)
 }
 
-func handleGetOne(w http.ResponseWriter, r *http.Request) {}
+func handleGetOne(w http.ResponseWriter, r *http.Request) {
+	env := r.PathValue("env")
+	key := r.PathValue("key")
+	log.Printf("Serving request for configuration value for env: %s\n", env)
 
-func handleSetMany(w http.ResponseWriter, r *http.Request) {}
+	value, ok := config.Get(config.Env(env), key)
+	if !ok {
+		log.Printf("Request for key '%s' failed\n", key)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(struct {
+		Value string `json:"value"`
+	}{
+		Value: value,
+	})
+}
+
+func handleSetMany(w http.ResponseWriter, r *http.Request) {
+	env := r.PathValue("env")
+	log.Printf("Serving request for setting configuration value for env: %s\n", env)
+}
 
 func main() {
 	http.HandleFunc("POST /api/v1/{env}", handleSetMany)
